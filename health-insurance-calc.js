20cb92ff4c25b29db94e9ad23ad7af365faa8e83
Jacob <jacobwolniak@lewisu.edu>
2021-03-10 19:42:04 -0600

    Basic Outline for program

---
+++ health-insurance-calc.js
@@ -0,0 +1,38 @@
+// this program will calculate and print the risk factor 
+// for the health insurance
+
+total = 0;
+
+function calcAgePoints() {
+    
+}
+
+function calcHeightPoints() {
+
+}
+
+function calcWeightPoints() {
+
+}
+
+function calcSysBP() {
+
+}
+
+function calcDiaBP() {
+
+}
+
+function diabetes() {
+
+}
+
+function cancer() {
+
+}
+
+function alzheimers() {
+
+}
+
+
